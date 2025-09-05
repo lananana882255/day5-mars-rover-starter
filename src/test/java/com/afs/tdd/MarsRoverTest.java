5e7e1df2a01716449ce3114a6cc9021d02633545
LANA.BI <LANA.BI@OOCL.COM>
2025-09-06 02:05:26 +0800

    test: execute command B

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -77,4 +77,26 @@ class MarsRoverTest {
         assertEquals(expectedDirection,result.getDirection());
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, N, 0, -1, N",
+            "0, 0, S, 0, 1, S",
+            "0, 0, W, 1, 0, W",
+            "0, 0, E, -1, 0, E"
+    })
+    public void Should_return_MarsRoverLocation_when_execute_command_given_command_B(
+            int initialLocationX, int initialLocationY, Direction initialDirection,
+            int expectedLocationX, int expectedLocationY, Direction expectedDirection) {
+        //given
+        String command="B";
+        MarsRover result = new MarsRover(initialLocationX,initialLocationY,initialDirection);
+
+        //when
+        result.executeCommand(command);
+
+        //then
+        assertEquals(expectedLocationX,result.getLocationX());
+        assertEquals(expectedLocationY,result.getLocationY());
+        assertEquals(expectedDirection,result.getDirection());
+    }
 }
