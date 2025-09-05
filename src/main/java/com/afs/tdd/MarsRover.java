df51edb166ca0fad6f8d0120b7c20782bca1a884
LANA.BI <LANA.BI@OOCL.COM>
2025-09-06 01:43:44 +0800

    feat: implement move function

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,21 +1,56 @@
 package com.afs.tdd;
 
 public class MarsRover {
-    private Location location;
-    public MarsRover(Location location) {
+    private int locationX;
+    private int locationY;
+    private Direction direction;
+
+    public MarsRover(int initialLocationX,int initialLocationY,Direction initialDirection) {
+        this.locationX=initialLocationX;
+        this.locationY=initialLocationY;
+        this.direction=initialDirection;
     }
 
     public int getLocationX() {
-        return 1;
+        return this.locationX;
     }
     public int getLocationY() {
-        return 1;
+        return this.locationY;
     }
     public Direction getDirection() {
-        return Direction.S;
+        return this.direction;
+    }
+    private void increaseLocationX() {
+        this.locationX+=1;
+    }
+    private void DecreaseLocationX() {
+        this.locationX-=1;
+    }
+    private void increaseLocationY() {
+        this.locationY+=1;
+    }
+    private void DecreaseLocationY() {
+        this.locationY-=1;
     }
 
     public void executeCommand(String command) {
+        if(command.equals("M")){
+            move();
+        }
+    }
 
+    private void move() {
+        if(this.direction==Direction.E){
+            increaseLocationX();
+        }
+        if(this.direction==Direction.W){
+            DecreaseLocationX();
+        }
+        if(this.direction==Direction.N){
+            increaseLocationY();
+        }
+        if(this.direction==Direction.S){
+            DecreaseLocationY();
+        }
     }
 }
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -1,33 +1,26 @@
 package com.afs.tdd;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
-    public static Stream<Arguments> provideTestCasesForMoveCommand() {
-        return Stream.of(
-                Arguments.of(0,0,Direction.N,0,1,Direction.N),
-                Arguments.of(0,0,Direction.S,0,-1,Direction.S),
-                Arguments.of(0,0,Direction.E,1,0,Direction.E),
-                Arguments.of(0,0,Direction.W,-1,0,Direction.W)
-        );
-    }
 
     @ParameterizedTest
-    @MethodSource("provideTestCasesForMoveCommand")
+    @CsvSource({
+            "0, 0, N, 0, 1, N",
+            "0, 0, S, 0, -1, S",
+            "0, 0, W, -1, 0, W",
+            "0, 0, E, 1, 0, E"
+    })
     public void Should_return_MarsRoverLocation_when_execute_command_given_command_M(
             int initialLocationX, int initialLocationY, Direction initialDirection,
             int expectedLocationX, int expectedLocationY, Direction expectedDirection) {
         //given
         String command="M";
-        Location location = new Location(initialLocationX,initialLocationY,initialDirection);
-        MarsRover result = new MarsRover(location);
+        MarsRover result = new MarsRover(initialLocationX,initialLocationY,initialDirection);
 
         //when
         result.executeCommand(command);
